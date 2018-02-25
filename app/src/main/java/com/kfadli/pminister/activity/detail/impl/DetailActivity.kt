6f5172a7f68cfa7e5bc81c2cad99e05033156e1c
Karim Fadli <karimm.fadli@gmail.com>
2018-02-25 20:50:29 +0100

    Fix -> TabLayout and Review height content

--- app/src/main/java/com/kfadli/pminister/activity/detail/impl/DetailActivity.kt
+++ app/src/main/java/com/kfadli/pminister/activity/detail/impl/DetailActivity.kt
@@ -2,6 +2,9 @@ package com.kfadli.pminister.activity.detail.impl
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
 import android.view.Gravity
 import android.view.ViewGroup.LayoutParams
@@ -10,67 +13,94 @@ import com.kfadli.pminister.R
 import com.kfadli.pminister.activity.base.BaseActivity
 import com.kfadli.pminister.activity.detail.IDetailPresenter
 import com.kfadli.pminister.activity.detail.IDetailView
-import com.kfadli.pminister.activity.detail.adapter.ContentProductAdapter
 import com.kfadli.pminister.activity.detail.adapter.ImagePagerAdapter
+import com.kfadli.pminister.activity.detail.fragment.edito.EditoFragment
+import com.kfadli.pminister.activity.detail.fragment.edito.ReviewsFragment
 import com.kfadli.pminister.api.ProductsApiInterface
 import com.kfadli.pminister.response.ResultDetail
 import com.kfadli.pminister.util.currencyFormat
 import com.kfadli.pminister.util.filterUrlByFormat
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_detail.gallery_viewpager
-import kotlinx.android.synthetic.main.activity_detail.toolbar
-import kotlinx.android.synthetic.main.content_detail.best_price_txt
-import kotlinx.android.synthetic.main.content_detail.reviews_txt
-import kotlinx.android.synthetic.main.content_detail.score
-import kotlinx.android.synthetic.main.content_detail.title_txt
-import kotlinx.android.synthetic.main.content_detail.viewPager
-import kotlinx.android.synthetic.main.review.date_txt
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 import javax.inject.Inject
 
 
 class DetailActivity : BaseActivity<IDetailView, IDetailPresenter>(), IDetailView {
 
-  @Inject
-  lateinit var apiService: ProductsApiInterface
+    @Inject
+    lateinit var apiService: ProductsApiInterface
 
-  lateinit override var presenter: IDetailPresenter
-
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    AndroidInjection.inject(this)
-
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_detail)
-    setSupportActionBar(toolbar)
-
-    presenter = DetailPresenter(this, apiService)
+    lateinit override var presenter: IDetailPresenter
 
 
-    //prepare LayoutParams
-    val layoutParams = ViewPager.LayoutParams()
-    layoutParams.width = LayoutParams.MATCH_PARENT
-    layoutParams.height = LayoutParams.WRAP_CONTENT
-    layoutParams.gravity = Gravity.BOTTOM
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
 
-    //add ViewPagerIndicator gallery bottom
-    val viewPagerIndicator = ViewPagerIndicator(this)
-    viewPagerIndicator.selectedDotColor = Color.BLACK
-    gallery_viewpager.addView(viewPagerIndicator, layoutParams)
-  }
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
 
-  override fun onDataReceived(product: ResultDetail?) {
+        presenter = DetailPresenter(this, apiService)
 
-    val urls: List<String> = filterUrlByFormat(product?.images, "LARGE")
-    gallery_viewpager.adapter = ImagePagerAdapter(supportFragmentManager, urls)
 
-    title_txt.text = product!!.headline
-    reviews_txt.text = product.nbReviews.toString()
-    best_price_txt.text = currencyFormat().format(product.summaryBestPrice!!.toDouble())
+        //prepare LayoutParams
+        val layoutParams = ViewPager.LayoutParams()
+        layoutParams.width = LayoutParams.MATCH_PARENT
+        layoutParams.height = LayoutParams.WRAP_CONTENT
+        layoutParams.gravity = Gravity.BOTTOM
 
-    viewPager.adapter = ContentProductAdapter(supportFragmentManager, product)
-    score.rating = product.reviewsAverageNote?.toFloat()!!
-  }
+        //add ViewPagerIndicator gallery bottom
+        val viewPagerIndicator = ViewPagerIndicator(this)
+        viewPagerIndicator.selectedDotColor = Color.BLACK
+        gallery_viewpager.addView(viewPagerIndicator, layoutParams)
 
-  override fun onDataFailed() {
-  }
+        viewPager.setOnTouchListener({ v, event -> true })
+
+    }
+
+    override fun onDataReceived(product: ResultDetail?) {
+
+        val urls: List<String> = filterUrlByFormat(product?.images, "LARGE")
+        gallery_viewpager.adapter = ImagePagerAdapter(supportFragmentManager, urls)
+
+        title_txt.text = product!!.headline
+        reviews_txt.text = product.nbReviews.toString()
+        best_price_txt.text = currencyFormat().format(product.summaryBestPrice!!.toDouble())
+
+        tabs_layout.setupWithViewPager(viewPager)
+        viewPager.adapter = ContentProductAdapter(supportFragmentManager, product)
+
+        score.rating = product.reviewsAverageNote?.toFloat()!!
+    }
+
+    override fun onDataFailed() {
+    }
+
+    inner class ContentProductAdapter(fragmentManager: FragmentManager,
+                                      val product: ResultDetail) : FragmentStatePagerAdapter(fragmentManager) {
+
+        override fun getCount(): Int {
+            return 2
+        }
+
+        override fun getItem(position: Int): Fragment {
+            when (position) {
+                1 -> return ReviewsFragment.newInstance(product.reviews)
+                0 -> return EditoFragment.newInstance(product.edito!!)
+            }
+
+            return Fragment()
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            when (position) {
+                1 -> return getString(R.string.reviews).toUpperCase()
+                0 -> return getString(R.string.edito).toUpperCase()
+            }
+
+            return ""
+        }
+
+    }
 }
