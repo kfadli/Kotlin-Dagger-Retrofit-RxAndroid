b49903c174cf9596e2d641c18210355ec93d15f3
Karim Fadli <karimm.fadli@gmail.com>
2018-02-26 21:01:32 +0100

    Fix -> Date

--- app/src/main/java/com/kfadli/pminister/activity/detail/adapter/RecyclerReviewAdapter.kt
+++ app/src/main/java/com/kfadli/pminister/activity/detail/adapter/RecyclerReviewAdapter.kt
@@ -7,43 +7,41 @@ import android.view.ViewGroup
 import com.kfadli.pminister.R
 import com.kfadli.pminister.activity.detail.adapter.RecyclerReviewAdapter.ReviewHolder
 import com.kfadli.pminister.response.ReviewsItem
-import kotlinx.android.synthetic.main.review.view.date_txt
-import kotlinx.android.synthetic.main.review.view.description
-import kotlinx.android.synthetic.main.review.view.score
-import kotlinx.android.synthetic.main.review.view.sells_review_txt
-import kotlinx.android.synthetic.main.review.view.title_txt
-import java.util.Date
+import kotlinx.android.synthetic.main.review.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RecyclerReviewAdapter(
 
-    private val reviews: List<ReviewsItem?>) : RecyclerView.Adapter<ReviewHolder>() {
+        private val reviews: List<ReviewsItem?>) : RecyclerView.Adapter<ReviewHolder>() {
 
-  private val TAG: String = "RecyclerReviewAdapter"
+    private val TAG: String = "RecyclerReviewAdapter"
 
-  override fun getItemCount(): Int {
-    return reviews.size
-  }
-
-  override fun onBindViewHolder(holder: ReviewHolder, position: Int) {
-    holder.bind(reviews[position])
-  }
-
-  override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ReviewHolder {
-    return ReviewHolder(
-        LayoutInflater.from(parent?.context).inflate(R.layout.review, parent, false))
-  }
-
-  class ReviewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-
-    fun bind(review: ReviewsItem?) {
-      itemView.title_txt.text = review?.title ?: ""
-      itemView.sells_review_txt.text = review!!.author?.login ?: ""
-      itemView.score.rating = review.note?.toFloat() ?: 0.0f
-      itemView.description.text = review.description
-      itemView.date_txt.text = Date(review.date!!).toString()
+    override fun getItemCount(): Int {
+        return reviews.size
     }
 
-  }
+    override fun onBindViewHolder(holder: ReviewHolder, position: Int) {
+        holder.bind(reviews[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ReviewHolder {
+        return ReviewHolder(
+                LayoutInflater.from(parent?.context).inflate(R.layout.review, parent, false))
+    }
+
+    class ReviewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        private val sdf: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+
+        fun bind(review: ReviewsItem?) {
+            itemView.title_txt.text = review?.title ?: ""
+            itemView.sells_review_txt.text = review!!.author?.login ?: ""
+            itemView.score.rating = review.note?.toFloat() ?: 0.0f
+            itemView.description.text = review.description
+            itemView.date_txt.text = sdf.format(Date(review.date!!))
+        }
+
+    }
 
 }
 
