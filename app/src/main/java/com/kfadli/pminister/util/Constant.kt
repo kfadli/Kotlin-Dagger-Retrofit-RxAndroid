e6561546087278e1ac1edd5c07e8c7baa1f34b6f
Karim Fadli <karimm.fadli@gmail.com>
2018-02-28 18:24:57 +0100

    Cleaning dummy methods/variable

--- app/src/main/java/com/kfadli/pminister/util/Constant.kt
+++ app/src/main/java/com/kfadli/pminister/util/Constant.kt
@@ -9,7 +9,6 @@ object Constant {
 
   val IMAGE_URL = "image_url"
   val IMAGE_URLS = "image_urls"
-  val CAN_REDIRECT = "redirect"
 
   val ADVERTS_PARCELABLE = "adverts_parcelable"
   val REVIEWS_PARCELABLE = "reviews_parcelable"
